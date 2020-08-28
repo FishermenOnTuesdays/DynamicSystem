d11707251dee11df4cac7550fa79b480e4025463
Стас Ладыгин <ladygin_sa_1017@1511.ru>
2020-08-28 23:20:51 +0300

    Lyapunov demo_v1

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include<algorithm>
 #include<vector>
 #include<complex>
+#include<cmath>
 #include "fparser.hh"
 #include "Eigen/Dense"
 
@@ -12,18 +13,62 @@ typedef std::complex<long double> complex;
 const double x_0 = 0.1;
 const double y_0 = 0.1;
 long int max_time = 10;
-const double dt = 0.01;
+double dt = 0.01;
 const double dh = 0; //Шаг векторного поля
 const double E1 = 0.1;
 const double E2 = 0.1;
+const std::vector<std::string> Lorenz_attractor = { "10*(x2-x1)", "x1*(28-x3)-x2", "x1*x2-(8/3)*x3" };
+//const std::vector<std::string>  = {"10*(x2-x1)", "28*x1-x2-x1*x3-x1*x3"};
+const std::vector<std::string> fixed_point = { "-x3", "-x1", "-x2" };
 
-std::vector<std::string> functions/* = { "-x1*x1","10*x2*x2"}*/;
+size_t N;
+std::vector<std::string> functions;
+std::vector<long double> lyapunov_exponents;
+bool exact;
+bool is_attractor = false;
+Eigen::MatrixXd evolution_matrix;
 
 
 double x = x_0;
 double y = y_0;
 double h = 0;
-long int t = 0;																	
+long int t = 0;
+
+std::vector<long double> Sub(const std::vector<long double>& vec1, const std::vector<long double>& vec2)
+{
+	if (vec1.size() != vec2.size())
+		throw std::exception("Vectors aren't equal");
+	std::vector<long double> sub_vec(vec1.size());
+	for (size_t i = 0; i < vec1.size(); i++)
+		sub_vec[i] = vec1[i] - vec2[i];
+	return sub_vec;
+}
+
+std::vector<long double> Add(const std::vector<long double>& vec1, const std::vector<long double>& vec2)
+{
+	if (vec1.size() != vec2.size())
+		throw std::exception("Vectors aren't equal");
+	std::vector<long double> add_vec(vec1.size());
+	for (size_t i = 0; i < vec1.size(); i++)
+		add_vec[i] = vec1[i] + vec2[i];
+	return add_vec;
+}
+
+std::vector<long double> Mult(const std::vector<long double>& vec, long double scalar)
+{
+	std::vector<long double> mult_vec(vec.size());
+	for (size_t i = 0; i < vec.size(); i++)
+		mult_vec[i] = vec[i] * scalar;
+	return mult_vec;
+}
+
+long double LengthVector(const std::vector<long double>& vec)
+{
+	long double length = 0;
+	for (size_t i = 0; i < vec.size(); i++)
+		length += vec[i] * vec[i];
+	return sqrtl(length);
+}
 
 bool Equal(const std::vector<long double>& vec1, const std::vector<long double>& vec2, const long double eps = 0.000001)
 {
@@ -178,7 +223,29 @@ bool IsHard(const std::vector<long double>& coor, const long double eps = 0.0000
 	for (size_t i = 0; i < jacobian_matrix.size(); i++)
 		for (size_t j = 0; j < jacobian_matrix.size(); j++)
 			jacobian_matrix_new(i, j) = jacobian_matrix[i][j];
+	//if(is_attractor)
+		//evolution_matrix = jacobian_matrix_new * evolution_matrix;
 	auto eigenvalues = jacobian_matrix_new.eigenvalues();
+	//const double lyapunov_eps = 0.01;
+	//if (!lyapunov_exponents.empty())
+	//{
+	//	exact = true;
+	//	for (size_t i = 0; i < eigenvalues.size(); i++)
+	//		if (fabs(eigenvalues[i].real() - lyapunov_exponents[i]) > lyapunov_eps)
+	//		{
+	//			exact = false;
+	//			break;
+	//		}
+	//}
+	//else
+	//	exact = false;
+	//lyapunov_exponents.clear();
+	//for (size_t i = 0; i < eigenvalues.size(); i++)
+	//{
+	//	//std::cout << eigenvalues[i] << " ";
+	//	lyapunov_exponents.push_back((evolution_matrix.transpose()*evolution_matrix).eigenvalues()[i].real());
+	//}
+	////std::cout << std::endl;
 	std::vector<long double> abs_eigenvalues;
 	for (size_t i = 0; i < eigenvalues.size(); i++)
 	{
@@ -284,26 +351,85 @@ std::vector<long double> CountNextCoor(const std::vector<long double>& coor, con
 	dy = dt / 6 * (k1_y + 2 * k2_y + 2 * k3_y + k4_y);*/
 }
 
+void CountLogSum(std::vector<long double>& log_sum, const Eigen::MatrixXd& matrix_deviation)
+{
+	Eigen::HouseholderQR<Eigen::MatrixXd> qr(matrix_deviation);
+	Eigen::MatrixXd orthonormal_matrix_deviation = qr.householderQ();
+	//std::cout << "Deviation:\n" << matrix_deviation << std::endl;
+	//std::cout << "Orthonormal:\n" << orthonormal_matrix_deviation << std::endl;
+	for (size_t i = 0; i < N; i++)
+	{
+		Eigen::VectorXd current_row = matrix_deviation.row(i);
+		//std::cout << "Row " << i << " : " << matrix_deviation.row(i) << std::endl;
+		for (size_t j = 0; j < i; j++)
+		{
+			//std::cout << "Dot deviation_row " << i << " and orthonormal_row " << j << " : " << matrix_deviation.row(i).dot(orthonormal_matrix_deviation.row(j)) << std::endl;
+			current_row -= matrix_deviation.row(i).dot(orthonormal_matrix_deviation.row(j)) * orthonormal_matrix_deviation.row(j);
+			//std::cout << "Current row: " << current_row << std::endl;
+		}
+		//std::cout << "Current norm: " << current_row.norm() << std::endl;
+		log_sum[i] += std::logl(current_row.norm());
+	}
+}
+
+void CountNextCoorDeviation(matrix_double& var_deviation, const std::vector<std::string>& functions, const double dt)
+{
+	for (size_t i = 0; i < N; i++)
+		var_deviation[i] = CountNextCoor(var_deviation[i], functions, dt);
+	/*matrix_double var_deviation{ N, std::vector<long double>(N)};
+	for (size_t i = 0; i < N; i++)
+	{
+		for (size_t j = 0; j < N; j++)
+			var_deviation[i][j] = var[j] + matrix_deviation(i,j);
+		var_deviation[i] = CountNextCoor(var_deviation[i], functions, dt);
+	}
+	var = CountNextCoor(var, functions, dt);
+	for (size_t i = 0; i < N; i++)
+		for (size_t j = 0; j < N; j++)
+			matrix_deviation(i, j) = var_deviation[i][j] - var[j];*/
+}
+
+void CountDeviation(Eigen::MatrixXd& deviation, const matrix_double& var_deviation, const std::vector<long double>& var)
+{
+	for (size_t i = 0; i < N; i++)
+		for (size_t j = 0; j < N; j++)
+			deviation(i, j) = var_deviation[i][j] - var[j];
+}
+
 int main()
 {
-	std::ofstream f1out;
-	f1out.open("../res/result.csv");//Введите свой путь
+	std::ofstream f1out, f2out;
+	f1out.open("../wwwroot/output/result.csv");//Введите свой путь
+	f2out.open("../wwwroot/output/laypunov.csv");//Введите свой путь
 	std::vector<long double> var;
-	size_t N;
-	std::cin >> N;
+	#ifdef _DEBUG
+		N = 3;
+	#else
+		std::cin >> N;
+	#endif
 	for (size_t i = 1; i <= N; i++)
-	{
 		f1out << 'x' + std::to_string(i) + ',';
-	}
+	for (size_t i = 1; i <= N; i++)
+		f1out << 'l' + std::to_string(i) + ',';
 	f1out << "t\n";
 	functions.resize(N);
-	for (size_t i = 0; i < N; i++)
-		std::cin >> functions[i];
+	#ifdef _DEBUG
+		functions = fixed_point;
+	#else
+		for (size_t i = 0; i < N; i++)
+			std::cin >> functions[i];
+	#endif
 	var.resize(N);
-	for (size_t i = 0; i < N; i++)
-		std::cin >> var[i];
+	#ifdef _DEBUG
+		for (size_t i = 0; i < N; i++)
+			var[i] = 0.1;
+	#else
+		for (size_t i = 0; i < N; i++)
+			std::cin >> var[i];
+	#endif
 	std::vector<std::vector<long double>> dots;
 	std::cin >> max_time;
+	std::cin >> dt;
 	while(t < max_time)
 	{
 		dots.push_back(var);
@@ -311,10 +437,50 @@ int main()
 		{
 			f1out << std::to_string(var[i])+',';
 		}
-		f1out << std::to_string(t * dt)+'\n';
 		var = CountNextCoor(var, functions, dt);
+		f1out << std::to_string(t * dt) + '\n';
 		t++;
 	}
+
+	/*std::cout << "GOOD\n";
+	for (size_t i = 0; i < N; i++)
+		std::cout << var[i] << ", ";
+	std::cout << "\nGOOD\n";*/
+
+	long int M = 100000;
+	long int T = 100;
+	long double eps_deviation = 1;
+	std::vector<long double> deviation_x(var.size(), 0);
+	deviation_x[0] = eps_deviation;
+	std::vector<long double> deviation_y(var.size(), 0);
+	deviation_y[1] = eps_deviation;
+	std::vector<long double> var_deviation_x = Add(var, deviation_x);
+	std::vector<long double> var_deviation_y = Add(var, deviation_y);
+	long double log_sum_x = 0, log_sum_y = 0;
+	Eigen::MatrixXd deviation = Eigen::MatrixXd::Identity(N,N);
+	matrix_double var_deviation{ N, std::vector<long double>(N) };
+	for (size_t i = 0; i < N; i++)
+		for (size_t j = 0; j < N; j++)
+			var_deviation[i][j] = var[j] + deviation(i, j);
+	std::vector<long double> log_sum(N, 0);
+	
+	for (long unsigned int i = 0; i < M; i++)
+	{
+		for (long unsigned int j = 0; j < T; j++)
+		{
+			CountNextCoorDeviation(var_deviation, functions, dt);
+			var = CountNextCoor(var, functions, dt);
+		}
+		CountDeviation(deviation, var_deviation, var);
+		CountLogSum(log_sum, deviation);
+		Eigen::HouseholderQR<Eigen::MatrixXd> qr(deviation);
+		deviation = qr.householderQ();
+		for (size_t j = 0; j < N; j++)
+			for (size_t k = 0; k < N; k++)
+				var_deviation[j][k] = var[k] + deviation(j, k);
+		for (size_t j = 0; j < N; j++)
+			f2out << log_sum[j]/i/T << ",";
+		f2out << std::endl;
+	}
 	f1out.close();
-	std::cout << "result.csv";
 }
